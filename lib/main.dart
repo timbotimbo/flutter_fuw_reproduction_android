f4bc935b8df6c77da59983e8d5cd4947c30e367f
timbotimbo <timbotimbo@users.noreply.github.com>
2024-01-07 00:04:32 +0100

    Add Flutter project and Unity export.

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _enabled = false;
+
+  void _toggleUnity() {
+    setState(() {
+      _enabled = !_enabled;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: _enabled
+            ? Container(
+                // red background only shows when UnityWidget is transparent/invisible
+                color: Colors.red,
+                child: UnityWidget(
+                  onUnityCreated: (controller) => {},
+                ),
+              )
+            : null,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleUnity,
+        child: Center(
+          child: Text("Toggle"),
+        ),
+      ),
+    );
+  }
+}
